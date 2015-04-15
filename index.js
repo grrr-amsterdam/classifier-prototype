42448c4d82de7be5427dcf02b0b5bf5263f78698
Larixk <larixk@gmail.com>
2015-04-15 13:07:03 +0200

    Now with saving and everything

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+var mongoose = require("mongoose");
 var app = require('express')();
 
-app.set('port', process.env.PORT || 3000);
+var classifier = require('./lib/classifier');
 
+var mongoURI = process.env.MONGOLAB_URI;
+
+mongoose.connect(mongoURI);
+
+var QuestionModel = mongoose.model('Questions', new mongoose.Schema({
+	body: {
+		type: String, trim: true, required: true
+	},
+	tags: [
+		{type: String, trim: true}
+	]
+}));
+
+app.set('port', process.env.PORT || 3000);
 app.use(require('body-parser').json());
 
 app.get('/', function (req, res) {
@@ -13,8 +28,50 @@ app.get('/script.js', function (req, res) {
 });
 
 app.post('/getTags', function (req, res) {
-	res.send({
-		data: require('./lib/classifier').getClassifications(req.body.question)
+	QuestionModel.find({}).exec(function (err, result) {
+		if (err) {
+			res.send({data:[]});
+		}
+		var trainingData = {
+			data: result.map(function (q) {
+				return {
+					question: q.body,
+					tags: q.tags
+				};
+			})
+		};
+		res.send({
+			data: classifier(trainingData).getClassifications(req.body.question)
+		});
+	});
+});
+
+app.get('/questions', function(req, res) {
+	QuestionModel.find({}).exec(function (err, result) {
+		res.send({
+			questions: result
+		});
+	});
+});
+
+app.post('/questions', function (req, res) {
+	var q = new QuestionModel({
+		body: req.body.question,
+		tags: req.body.tags
+	});
+
+	q.save(function (err) {
+		res.send({
+			status: err ? "error" : "saved"
+		});
+	});
+});
+
+app.post('/clear', function (req, res) {
+	QuestionModel.remove({}, function(err) {
+		res.send({
+			status: err ? "error" : "cleared"
+		});
 	});
 });
 
--- public/script.js
+++ public/script.js
@@ -1,20 +1,61 @@
-function getTagsForTextarea() {
-	postAndReadJSON('/getTags', {question: textarea.value}, function (result) {
-		showTags(result.data);
-	});
-}
+var chosenTagList = document.querySelector('ul');
+var form = document.querySelector('form');
+var tagInput = document.querySelector('input');
+var textarea = document.querySelector('textarea');
 
-function showTags(tags) {
-	var tagList = document.querySelector('ol');
-	while (tagList.firstChild) {
-		tagList.removeChild(tagList.firstChild);
+function showPossibleTags(tags) {
+	var possibleTagList = document.querySelector('ol');
+	while (possibleTagList.firstChild) {
+		possibleTagList.removeChild(possibleTagList.firstChild);
 	}
 
 	tags.forEach(function (tag) {
 		var li = document.createElement('li');
 		li.innerHTML = tag.label + " (" + tag.value + ")";
-		tagList.appendChild(li);
+		possibleTagList.appendChild(li);
+		li.addEventListener("click", function () {
+			addTag(tag.label);
+		});
 	});
+}
+
+function getAddedTags() {
+	var existingTags = Array.prototype.slice.call(chosenTagList.childNodes);
+	return existingTags.map(function(existingTag) {
+		return existingTag.innerHTML;
+	}).filter(function (tag) {
+		return tag;
+	});
+}
+
+function removeTag(tag) {
+	var existingTags = Array.prototype.slice.call(chosenTagList.childNodes);
+	existingTags.forEach(function (existingTag) {
+		if (existingTag.innerHTML !== tag) {
+			return;
+		}
+		chosenTagList.removeChild(existingTag);
+	});
+}
+
+function addTag(tag) {
+	var existingTags = getAddedTags();
+	var alreadyAdded = false;
+	existingTags.forEach(function (existingTag) {
+		if (existingTag === tag) {
+			alreadyAdded = true;
+		}
+	});
+	if (alreadyAdded) {
+		return;
+	}
+
+	var li = document.createElement('li');
+	li.innerHTML = tag;
+	li.addEventListener("click", function () {
+		removeTag(tag);
+	});
+	chosenTagList.appendChild(li);
 }
 
 function postAndReadJSON(url, data, callback) {
@@ -30,6 +71,37 @@ function postAndReadJSON(url, data, callback) {
 
 	request.send(JSON.stringify(data));
 }
-var textarea = document.querySelector('textarea');
-textarea.addEventListener('keyup', getTagsForTextarea);
+
+textarea.addEventListener('keyup', function () {
+	postAndReadJSON('/getTags', {question: textarea.value}, function (result) {
+		showPossibleTags(result.data);
+	});
+});
+
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+	var tags = getAddedTags();
+	var question = textarea.value;
+	console.log(tags);
+	console.log(question);
+	if (!tags.length || !question) {
+		return;
+	}
+	postAndReadJSON('/questions', {
+		question: question,
+		tags: getAddedTags()
+	}, function (result) {
+		location.reload();
+	});
+});
+
+tagInput.addEventListener('keydown', function (e) {
+	var keyCode = event.keyCode ? event.keyCode : event.which ? event.which : event.charCode;
+	if (keyCode === 13) {
+		e.preventDefault();
+		var tag = tagInput.value;
+		addTag(tag);
+		tagInput.value = "";
+	}
+});
 
